65aaa0eaf760aae5a20774911bd02f1cba8e69aa
David Enikuomehin <davidenikuomehin1@gmail.com>
2022-05-19 00:08:29 +0100

    play round of game

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-let moves = ["rock", "paper", "scissors"];
+const moves = ["rock", "paper", "scissors"];
+const playerSelection = moves[0];
+const computerSelection = computerPlay();
 
+console.log(playRound(playerSelection, computerSelection))
+
+//gets random input of moves from computer
 function computerPlay() {
-    let movesLength = moves.length;
-    let result = Math.floor(Math.random() * movesLength);
-    moves = moves[result];
-    return moves;
+    let lengthOfMoves = moves.length;
+    let random = Math.floor(Math.random() * lengthOfMoves);
+    let result = moves[random];
+    return result;
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == moves[0] && computerSelection == moves[1]) {
+        console.log("You Lose! Paper beats");
+    } else {
+        console.log("uh-oh. not looking good");
+    }
+    return
+}
+
