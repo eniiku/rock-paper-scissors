2d817d206278a3745363740ca890bc5e1906acfb
David Enikuomehin <davidenikuomehin1@gmail.com>
2022-06-03 04:36:16 +0100

    update all <unknown>

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const moves = ["rock", "paper", "scissors"];
-let playerSelection;
+const rock = document.querySelector('#rock')
+const paper = document.querySelector('#paper')
+const scissors = document.querySelector('#scissors')
+const playerSelection = [rock, paper, scissors]
 const computerSelection = computerPlay();
+const playerMoves = document.querySelector('.player-moves');
+let userWin = 0;
+let userLose = 0;
 
 //gets random input of moves from computer
 function computerPlay() {
@@ -12,6 +18,24 @@ function computerPlay() {
 
 //plays rounds of rock paper scissors
 function playRound(playerSelection, computerSelection) {
+    document.querySelector('.player-moves').addEventListener
+    ('click', function(e) {
+        const play = e.target.getAttribute('id');
+    
+        if (play == (moves[0])) {
+            console.log(play)
+            playerSelection = playerSelection[0];
+        }
+        if (play == (moves[1])) {
+            console.log(play)
+            playerSelection = moves[1];
+        }
+        if (play == (moves[2])) {
+            console.log(play)
+            playerSelection = moves[2];
+        }
+    });
+
     if (playerSelection == moves[0] && computerSelection == moves[2]) {
         console.log("You Win! Rock beats Scissors")
     } else if (playerSelection == moves[1] && computerSelection == moves[0]) {
@@ -29,23 +53,28 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
+
 //collects functions to simulate game
 function game() {
-            if (playerSelection == moves[0] && computerSelection == moves[0] || 
-                playerSelection == moves[0] && computerSelection == moves[0] || 
-                playerSelection == moves[0] && computerSelection == moves[0]) {
-                    alert("its a tie")
-                }
-            displayScores();
-}
+    playerSelection.forEach(btn => {
+        btn.addEventListener('click', () => {
+            playRound(playerSelection, computerSelection);
+            displayScores();   
+        
+            if (userWin < 5 || userLose < 5) {
+            }
+            displayWinner();
+        })
+    });
+};
+
+game();
 
 //displays scores and result of game
-function displayScores() {
-    let userWin = 0;
-    let userLose = 0;
+function displayScores() { 
     let userScore = document.querySelector('.user-win');
     let uiScore = document.querySelector('.user-lose');
-    
+
     if (playerSelection == moves[0] && computerSelection == moves[2] || 
         playerSelection == moves[1] && computerSelection == moves[0] || 
         playerSelection == moves[2] && computerSelection == moves[1]) {
@@ -54,32 +83,16 @@ function displayScores() {
             playerSelection == moves[0] && computerSelection == moves[1] || 
             playerSelection == moves[1] && computerSelection == moves[2]) {
                 userLose++;
-            }
+            } 
             
     userScore.textContent = userWin;
     uiScore.textContent = userLose;
-            
+}
+
+function displayWinner() {
     if (userWin > userLose) {
         console.log("You: " + userWin + " " + "Computer: " + userLose +"\nYou Win!!")
     } else {
         console.log("You: " + userWin + " " + "Computer: " + userLose +"\nOh no! You lost.")
     }
 }
-
-
-// plays game based on user input 
-document.querySelector('#player-moves').addEventListener
-('click', function(e) {
-    const play = e.target.getAttribute('id');
-
-    if (play == (moves[0])) {
-        playerSelection = moves[0];
-        game();
-    } else if (play == (moves[1])) {
-        playerSelection = moves[1];
-        game();
-    } else {
-        playerSelection = moves[2];
-        game();
-    }
-});
